2a73d648a5623a8da1eba41e46ba65a4c04839b0
dameeta <dameeta835@gmail.com>
2023-07-07 12:41:35 +0530

    first

---
+++ src/components/header/spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import { Header } from './index';
+
+
+describe('Header Component',()=>{
+    it('It should render without errors',()=>{
+
+        expect(1).toBe(3);
+    })
+})
+/* if('It should render the function without errors',()=>{
+
+    expect(1).toBe(3);
+}); */
